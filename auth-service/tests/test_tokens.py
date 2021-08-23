c981a97e04904d416845e4969487be2d442b9d35
Michael-Hathaway <mikehath5@gmail.com>
2021-08-23 13:40:25 -0700

    updated unittests for JsonWebTokens class

--- auth-service/tests/test_tokens.py
+++ auth-service/tests/test_tokens.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-from utils.tokens import TokenManager
+from utils.tokens import JsonWebToken
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
@@ -9,7 +9,7 @@ from cryptography.hazmat.backends import default_backend
 class MockRSAKeys:
     """
     Helper class for generating RSA public/private key pairs to be used
-    when testing the TokenManager class
+    when testing the JsonWebToken class
     """
 
     @staticmethod
@@ -45,93 +45,80 @@ class TestMockRSAKeys(unittest.TestCase):
 
 
 class TestTokenManager(unittest.TestCase):
-    """Test TokenManager Class"""
+    """Test JsonWebToken Class"""
 
     @classmethod
     def setUpClass(cls):
-        """Set environment variables before running tests"""
+        """instantiate JsonWebToken singleton before running tests"""
         public_key, private_key = MockRSAKeys.get_key_pair()
-        os.environ['TOKEN_PRIVATE_KEY'] = private_key
-        os.environ['TOKEN_PUBLIC_KEY'] = public_key
+        JsonWebToken(public_key, private_key)
 
     def test_can_get_singleton_instance(self):
-        """Test that we can get the singleton instance for the TokenManager"""
-        first_instance = TokenManager.get_instance()
+        """Test that we can get the singleton instance for the JsonWebToken"""
+        first_instance = JsonWebToken.get_instance()
         self.assertIsNotNone(first_instance)
 
-        second_instance = TokenManager.get_instance()
+        second_instance = JsonWebToken.get_instance()
         self.assertIsNotNone(second_instance)
 
     def test_instance_is_singleton_instance(self):
         """Test that the singleton instances are the same"""
-        first_instance = TokenManager.get_instance()
-        second_instance = TokenManager.get_instance()
+        first_instance = JsonWebToken.get_instance()
+        second_instance = JsonWebToken.get_instance()
 
         self.assertIs(first_instance, second_instance)
-
-    def test_token_manager_load_private_key(self):
-        """Test that the TokenManager loads private key environment variable"""
-
-        # ensure that the TOKEN_PRIVATE_KEY environment variable is set
-        env_private_key = os.environ.get('TOKEN_PRIVATE_KEY', None)
-        msg = 'TOKEN_PRIVATE_KEY environment variable has not been set'
-        self.assertIsNotNone(env_private_key, msg)
-
-        # Get the token manager instance
-        tokenManager = TokenManager.get_instance()
-
-        # get the private key from the instance
-        private_key = tokenManager._get_private_key()
-        msg = f'self._private_token is None, but should be {env_private_key}'
-        self.assertIsNotNone(private_key)
-
-        # check that keys match
-        msg = 'TokenManager private key does not match environment variable'
-        self.assertEqual(env_private_key, private_key)
-
-    def test_token_manager_load_public_key(self):
-        """Test that the TokenManager loads public key environment variable"""
-
-        # ensure that the TOKEN_PUBLIC_KEY environment variable is set
-        env_public_key = os.environ.get('TOKEN_PUBLIC_KEY', None)
-        msg = 'TOKEN_PUBLIC_KEY environment variable has not been set'
-        self.assertIsNotNone(env_public_key, msg)
-
-        # Get the token manager instance
-        tokenManager = TokenManager.get_instance()
-
-        # get the public key from the instance
-        public_key = tokenManager.get_public_key()
-        msg = f'self._public_token is None, but should be {env_public_key}'
-        self.assertIsNotNone(public_key)
-
-        # check that keys match
-        msg = 'TokenManager public key does not match environment variable'
-        self.assertEqual(env_public_key, public_key)
 
     def test_can_create_a_token(self):
         """Test that we can create a JWT"""
 
-        tokenManager = TokenManager.get_instance()
-        payload = "test token data"
+        tokenManager = JsonWebToken.get_instance()
+        payload = payload = {'data': 'test token data'}
         timeToLiveInSeconds = 1500
 
-        token = tokenManager.create(payload, timeToLiveInSeconds)
+        tokenId, token = tokenManager.create(payload, timeToLiveInSeconds)
+        self.assertIsNotNone(tokenId, 'Could not create token')
         self.assertIsNotNone(token, 'Could not create token')
 
     def test_can_decode_token_after_creation(self):
         """Test that JWT can be decoded to get original data"""
 
-        tokenManager = TokenManager.get_instance()
-        payload = "test token data"
+        tokenManager = JsonWebToken.get_instance()
+        payload = {'data': 'test token data'}
         timeToLiveInSeconds = 1500
 
-        token = tokenManager.create(payload, timeToLiveInSeconds)
+        tokenId, token = tokenManager.create(payload, timeToLiveInSeconds)
         self.assertIsNotNone(token, 'Could not create token')
 
         decoded_payload = tokenManager.decode(token)
-        msg = 'Decoded payload does not match original payload'
-        self.assertEqual(payload, decoded_payload, msg)
+        msg = 'Decoded payload data does not match original payload'
+        self.assertEqual(payload['data'], decoded_payload['data'], msg)
+
+        msg = 'Decoded payload tokenId does not match original payload'
+        self.assertEqual(tokenId, decoded_payload['tokenId'], msg)
+
+    def test_can_deconde_without_verification(self):
+        """
+        Test that JWT can be decoded without verification
+        to get original data
+        """
+        tokenManager = JsonWebToken.get_instance()
+        payload = {'data': 'test token data'}
+        # set TTL to -100 so that it is already expired
+        timeToLiveInSeconds = -100
+
+        tokenId, token = tokenManager.create(payload, timeToLiveInSeconds)
+        self.assertIsNotNone(token, 'Could not create token')
+
+        invalid_payload = tokenManager.decode(token)
+        msg = 'Token should not be able to be decoded with verification'
+        self.assertIsNone(invalid_payload, msg)
+
+        valid_payload = tokenManager.decode_without_verification(token)
+        msg = 'Decoded payload data does not match original payload'
+        self.assertEqual(payload['data'], valid_payload['data'], msg)
+
+        msg = 'Decoded payload tokenId does not match original payload'
+        self.assertEqual(tokenId, valid_payload['tokenId'], msg)
 
 
 if __name__ == '__main__':
